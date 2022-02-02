73dbb67a2753088f68f5f866a8bf82d4e944f637
Anthony9698 <anthonyviera4@gmail.com>
2022-02-02 13:20:21 -0600

    Set up email and password fields for signin form

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/auth/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: SigninComponent,
+    path: 'signin',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
