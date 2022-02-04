d49a1559aeb72ec0e3d937a39f9204af04458270
Anthony9698 <anthonyviera4@gmail.com>
2022-02-04 13:46:47 -0600

    Added password match validator and added form styling for valid/invalid first name input

--- app/src/app/components/auth/register/register.component.ts
+++ app/src/app/components/auth/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { User } from 'src/app/models/user/user';
 
 @Component({
@@ -11,18 +17,71 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   newUser: User;
   confirmPassword: string;
+  submitted: boolean;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.newUser = new User('', '', '', '');
+    this.confirmPassword = '';
+    this.submitted = false;
   }
 
   ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      firstName: new FormControl(this.newUser.firstName),
-      lastName: new FormControl(this.newUser.lastName),
-      email: new FormControl(this.newUser.email),
-      password: new FormControl(this.newUser.password),
-      confirmPassword: new FormControl(this.confirmPassword),
+    this.registerForm = this.formBuilder.group({
+      firstName: [this.newUser.firstName, [Validators.required]],
+      lastName: [this.newUser.lastName, [Validators.required]],
+      email: [this.newUser.email, [Validators.required, Validators.email]],
+      passwordsForm: this.formBuilder.group(
+        {
+          password: [
+            this.newUser.password,
+            [
+              Validators.required,
+              Validators.minLength(8),
+              Validators.maxLength(25),
+            ],
+          ],
+          confirmPassword: [
+            this.confirmPassword,
+            [
+              Validators.required,
+              Validators.minLength(8),
+              Validators.maxLength(25),
+            ],
+          ],
+        },
+        {
+          validators: this.passwordsMatch('password', 'confirmPassword'),
+        }
+      ),
     });
+    console.log(this.submitted);
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.registerForm.get('firstName'));
+  }
+
+  passwordsMatch(controlName: string, matchingControlName: string) {
+    return (control: AbstractControl): ValidationErrors | null => {
+      let input = control.get(controlName);
+      let matchingInput = control.get(matchingControlName);
+
+      if (input === null || matchingInput == null) {
+        return null;
+      }
+
+      if (matchingInput.errors && !matchingInput.errors.confirmedValidator) {
+        return null;
+      }
+
+      if (input.value !== matchingInput.value) {
+        matchingInput.setErrors({ confirmedValidator: true });
+        return { confirmedValidator: true };
+      } else {
+        matchingInput.setErrors(null);
+        return null;
+      }
+    };
   }
 }
